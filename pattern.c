0f43fa0c3dff996a6a0e672eba5436cb3e84804b
Sai Vishnu <saivishnu725@gmail.com>
2021-09-02 22:55:12 +0530

    Added ASCII conversion

--- pattern.c
+++ pattern.c
@@ -3,14 +3,16 @@
 int main()
 {
     int lines, line_count, count;
+    char letter;
     lines = 7;
-    for (line_count = lines; line_count >= 1; line_count--)
+    for (line_count = lines-1; line_count >= 0; line_count--)
     {
         count = first + line_count;
         for (int i = first; i <= count; i++)
         {
-
-            printf("%d ", i);
+            letter = (char)i;
+            // printf("%d ", i);
+            printf("%c ", letter);
         }
         printf("\n");
     }

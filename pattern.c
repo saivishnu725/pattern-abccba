b0522b2897781d2c6f58f81cebe0941607afddd3
Sai Vishnu <saivishnu725@gmail.com>
2021-09-03 00:05:36 +0530

    Almost done

--- pattern.c
+++ pattern.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#define first 97
+#define first 65
 int main()
 {
-    int lines, line_count, count;
+    int lines, line_count, count, space_count_line_number;
     char letter;
     lines = 7;
+    space_count_line_number = 0;
     for (line_count = lines - 1; line_count >= 0; line_count--)
     {
         count = first + line_count;
@@ -12,28 +13,23 @@ int main()
         {
             letter = (char)i;
             // printf("%d ", i);
-            printf("%c ", letter);
+            printf("%c", letter);
         }
-        printf("    ");
+
+        for (int i = 0; i < space_count_line_number; i++)
+        {
+            printf(" ");
+        }
+
         for (int i = count; i >= first; i--)
         {
             letter = (char)i;
             // printf("%d ", i);
-            printf("%c ", letter);
+            printf("%c", letter);
         }
 
         printf("\n");
+        space_count_line_number += 2;
     }
-    // for (line_count = 0; line_count < lines; line_count++)
-    // {
-    //     count = first + line_count;
-    //     for (int i = count; i >= first; i--)
-    //     {
-    //         letter = (char)i;
-    //         // printf("%d ", i);
-    //         printf("%c ", letter);
-    //     }
-    //     printf("\n");
-    // }
     return 0;
 }

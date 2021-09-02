905406fc8eb204dd1b933efa67a7820f646a35e8
Sai Vishnu <saivishnu725@gmail.com>
2021-09-02 23:32:44 +0530

    Last (Third) part done

--- pattern.c
+++ pattern.c
@@ -5,7 +5,7 @@ int main()
     int lines, line_count, count;
     char letter;
     lines = 7;
-    for (line_count = lines-1; line_count >= 0; line_count--)
+    for (line_count = lines - 1; line_count >= 0; line_count--)
     {
         count = first + line_count;
         for (int i = first; i <= count; i++)
@@ -14,8 +14,26 @@ int main()
             // printf("%d ", i);
             printf("%c ", letter);
         }
+        printf("    ");
+        for (int i = count; i >= first; i--)
+        {
+            letter = (char)i;
+            // printf("%d ", i);
+            printf("%c ", letter);
+        }
+
         printf("\n");
     }
-
+    // for (line_count = 0; line_count < lines; line_count++)
+    // {
+    //     count = first + line_count;
+    //     for (int i = count; i >= first; i--)
+    //     {
+    //         letter = (char)i;
+    //         // printf("%d ", i);
+    //         printf("%c ", letter);
+    //     }
+    //     printf("\n");
+    // }
     return 0;
 }

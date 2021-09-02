e71fe43fed023712333efa13ed1d1e80146db5f7
Sai Vishnu <saivishnu725@gmail.com>
2021-09-02 22:51:21 +0530

    First part done

--- pattern.c
+++ pattern.c
@@ -1,35 +1,19 @@
 #include <stdio.h>
-#define FIRST 97
-
+#define first 97
 int main()
 {
-    int i, j, k, letter, line1, line2, lines, count, number, space;
-    // printf("Enter the number of lines: ");       // Uncomment at the end!
-    // scanf("%d",&lines);                          // Uncomment at the end!
-    lines = 7; // Will remove this later
-    line1 = lines;
-    line2 = lines;
-    count = FIRST + lines;
-    for (i = FIRST; i < count; i++)
+    int lines, line_count, count;
+    lines = 7;
+    for (line_count = lines; line_count >= 1; line_count--)
     {
-        number = i;
-        for (j = 0; j < line1; j++)
+        count = first + line_count;
+        for (int i = first; i <= count; i++)
         {
-            letter = (char)number;
-            // printf("%d,%d,%c,%d    ", i, j, letter, number);
-            printf("%c", letter);
-            number = number + 1;
+
+            printf("%d ", i);
         }
-        for ( k = count; k >= FIRST; k--)
-        {
-            
-        }
-        
         printf("\n");
-        line1--;
     }
-    printf("\n");
-    line1 = 0;
 
     return 0;
 }
